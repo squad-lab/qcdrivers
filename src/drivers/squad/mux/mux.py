974b3c5a775ac0272ad84efd101b2600dd574918
Janitor <squad@fz-juelich.de>
2024-08-15 15:14:26 +0000

    black update

--- src/drivers/squad/mux/mux.py
+++ src/drivers/squad/mux/mux.py
@@ -27,8 +27,8 @@ N_y = len(y_pins)
 class Muxi(VisaInstrument):
     """
     The Muxi class is used to control an AD75019 crosspoint switch
-    using a serial interface via pyVISA and QCoDeS. This class provides methods for 
-    configuring the switch, switching connections, querying connection statuses, 
+    using a serial interface via pyVISA and QCoDeS. This class provides methods for
+    configuring the switch, switching connections, querying connection statuses,
     and visualizing the switch matrix status.
     """
 
@@ -57,8 +57,12 @@ class Muxi(VisaInstrument):
         self.switch_matrix = np.zeros((32, 32), dtype=int)
 
         # Define the valid X and Y pins (excluding the disabled range)
-        self.x_pins = [i for i in range(32) if i not in {12, 13, 14, 15, 28, 29, 30, 31}]
-        self.y_pins = [j for j in range(32) if j not in {12, 13, 14, 15, 28, 29, 30, 31}]
+        self.x_pins = [
+            i for i in range(32) if i not in {12, 13, 14, 15, 28, 29, 30, 31}
+        ]
+        self.y_pins = [
+            j for j in range(32) if j not in {12, 13, 14, 15, 28, 29, 30, 31}
+        ]
 
     def configure_pins(self, pclk_pin, sclk_pin, sin_pin):
         """
@@ -107,14 +111,14 @@ class Muxi(VisaInstrument):
             return "Route skipped: disabled range"
 
         # Construct the command to add the route
-        command = f'SWITCH32 {x},{y}'
+        command = f"SWITCH32 {x},{y}"
         self.send_command(command)
 
         # Flush the configuration to apply the changes
-        response = self.send_command('FLUSH')
+        response = self.send_command("FLUSH")
 
         return response
-    
+
     def switch16(self, x, y):
         """
         Equivalent to adding a route and flushing the configuration.
@@ -128,11 +132,11 @@ class Muxi(VisaInstrument):
         """
 
         # Construct the command to add the route
-        command = f'SWITCH16 {x},{y}'
+        command = f"SWITCH16 {x},{y}"
         self.send_command(command)
 
         # Flush the configuration to apply the changes
-        response = self.send_command('FLUSH')
+        response = self.send_command("FLUSH")
 
         return response
 
@@ -174,7 +178,7 @@ class Muxi(VisaInstrument):
         Returns:
             configuration matrix as a string.
         """
-        self.send_command('PRINT')
+        self.send_command("PRINT")
         response = self.visa_handle.read()
         return response
 
@@ -202,7 +206,9 @@ class Muxi(VisaInstrument):
         for i in range(N_x):
             for j in range(N_y):
                 if (i, j) not in switchMatrix:
-                    switchMatrix[i, j] = 1  # Example logic; replace with actual switch status check
+                    switchMatrix[i, j] = (
+                        1  # Example logic; replace with actual switch status check
+                    )
 
         if plot:
             fig, ax = plt.subplots()
