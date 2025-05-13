fd0fc02faa0c473677c576dfe5542fa8ea8f7f7b
Janitor <squad@fz-juelich.de>
2025-05-13 12:32:42 +0000

    ruff update

--- src/drivers/entropy/__init__.py
+++ src/drivers/entropy/__init__.py
@@ -1,6 +1,4 @@
-from drivers.entropy.adr import adr 
+from drivers.entropy.adr import adr
 from drivers.entropy.adr.adr import ADR
-from drivers.entropy.heater import heater 
+from drivers.entropy.heater import heater
 from drivers.entropy.heater.heater import Heater
-
-
